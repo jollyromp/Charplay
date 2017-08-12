1560f1c980c7d77f80fa39ae943717e417f5df5a
jollyromp <jollyromp@gmail.com>
2017-08-12 14:46:15 -0700

    Add MongoDB

--- server.js
+++ server.js
@@ -12,3 +12,15 @@ io.on('connection', (client) => {
 const port = 8000;
 io.listen(port);
 console.log('listening on port ', port);
+
+// MongoDB Connection
+
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+
+var url = 'mongodb://localhost:27017/test';
+MongoClient.connect(url, function(err, db) {
+  assert.equal(null, err);
+  console.log("Connected correctly to server.");
+  db.close();
+});
