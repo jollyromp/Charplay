b9211b0b445b8fc9d4c70242749c8ae2b5ee7267
Ubuntu <ubuntu@ip-172-31-14-84.us-west-2.compute.internal>
2017-08-17 03:14:36 +0000

    Seperating components and changing project name

--- src/App.js
+++ src/App.js
@@ -1,64 +1,16 @@
 import React, { Component } from 'react';
 import './App.css';
 
-import { roomSubscribe } from './api';
+import RoomContainer from './room/RoomContainer'
 
 class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {room: '', messages: []};
-    roomSubscribe((err, data) => this.handleSocketData(err, data));
-  }
-
-  handleSocketData = (err, data) => {
-    if (data.room) {
-      this.setState({room: data.room});
-    }
-    
-    if (data.messages) {
-      var tempMessages = this.state.messages;
-      tempMessages = tempMessages.concat(data.messages);
-      this.setState({messages: tempMessages});
-      console.log(data.messages);
-      console.log(this.state.messages);
-    }
-  }
-
   render() {
-    
-    const messageList = this.state.messages.map((message) => <Message key={message._id} value={message} />);
-
     return (
       <div>
-        <div className='room-information'>
-          <h1>{this.state.room.name}</h1>
-          <div>{this.state.room.description}</div>
-        </div>
-
-        <div className='messages'>
-          {messageList}
-        </div>
-
+        <RoomContainer name='test' />
       </div>
     );
   }
-}
-
-function Message(props) {
-  return (
-    <div className='message' style={{color: props.value.characterColor}}>
-
-      <div>
-        <img alt={props.value._character.name + '-avatar'} src={props.value._character.avatar} height='100' />
-        <h2>{props.value._character.name}</h2>
-      </div>
-
-      <div>
-        {props.value.content}
-      </div>
-
-    </div>
-  );
 }
 
 export default App;
