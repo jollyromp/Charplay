054fe4ec5584e1c80c7afdad2289efa49caf0c92
Cor <jollyromp@gmail.com>
2017-08-18 09:14:53 +0000

    updating websocket URI

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 import openSocket from 'socket.io-client';
-const  socket = openSocket('http://54.187.38.171:8000');
+const  socket = openSocket('http://dev.charplay.com:8000');
 
 function roomSubscribe(roomId, cb) {
   socket.on('roomInfo', data => cb(null, data));
