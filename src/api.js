19e27fb9b31c924a43e690a49130d9bb28b2dfd5
Cor <jollyromp@gmail.com>
2017-08-24 03:22:30 +0000

    react-scripts relay

--- src/api.js
+++ src/api.js
@@ -3,6 +3,9 @@ const  io = openSocket('ws://dev.charplay.com:8000');
 
 var moment = require('moment');
 
+import { graphql } from 'relay-runtime';
+graphql`query {user(_id:"59988d2f5c222a4bdbae13e8"){username,password}}`;
+
 function roomSubscribe(roomUrl, cb) {
   if (roomUrl) {
     io.on('sendRoomInfo', data => cb(null, data));
